bc4554d4439f976e58e33cea2aa6b9e4b38f9c69
juan pablo aranda herreño <kingnero199412@gmail.com>
2022-09-22 10:02:42 -0500

    cambios de clase de introduccion a react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-const estilo2 = {
-  boxSadow: '0 5px 3px rgba(0,0,0,0.5)'
-}
-const estilo = ({bg = '#222'}) =>({
-  backgroundColor: bg,
-  color: '#fff',
-  padding: '10px 15px',
-  margin: '10px 15px'
-})
-const Li = ({children}) =>{
-  return(
-    <li style={{...estilo2,...estilo({bg: '#333'})} }class='clase-li'>{children}</li>
+import Button from "./Button"
+
+const arr = [
+    'chachito feliz',
+    'chachito triste',
+    'chachito emocionado'
+]
+
+function App() {
+    const miVariable = false
+    if(miVariable){
+        return <p>Mi variable dio true!</p>
+    }
+  return (
+    <div>
+        <h1 onClick={(e)=> console.log('click',e)}>Hola mundo</h1>
+        {arr.map(el => <p key={el}>{el}</p>)}
+        <Button onClick={()=>console.log('clickeado')} >
+            Enviar
+        </Button>
+    </div>
   )
 }
-const App = () =>{
-  const valor = 'triste'
-  return (
-   <ul style={estilo({bg: '#750'})} className='clase-css'>
-    <Li estado='feliz'>valor de li</Li>
-   </ul>
-  );
-}
 
-export default App;
+export default App
---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import './Button.css'
+
+function Button(props) {
+  return (
+    <button {...props} className="btn" />
+  )
+}
+
+export default Button
